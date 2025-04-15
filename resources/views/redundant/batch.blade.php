7cc67c0a37af9b0f85ad982c038bfb2372f63a3c
Ryal02 <ryan.basadre@fligno.com>
2025-04-15 17:38:19 +0800

    update 2.4

--- resources/views/redundant/batch.blade.php
+++ resources/views/redundant/batch.blade.php
@@ -55,6 +55,7 @@
                             <th>Batch</th>
                             <th>Dependent Name</th>
                             <th>Age</th>
+                            <th>precint</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -65,6 +66,7 @@
                                     <td>{{ $dependent->batch_belong }}</td>
                                     <td>{{ $dependent->dependents }}</td>
                                     <td>{{ $dependent->dep_age ?? 'N/A' }}</td>
+                                    <td>{{ $dependent->dep_d2 ? $dependent->dep_d2 : $dependent->dep_d1  }}</td>
                                 </tr>
                         @endforeach
                     </tbody>
